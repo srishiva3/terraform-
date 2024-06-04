ed7b028a25e0adf5be2d2b60eb535b1798a6f113
srishivajothish <81418992+srishiva3@users.noreply.github.com>
2024-06-04 08:25:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_mysql_flexible_server" "example" {
-  name                = "my_mysql_server"  # Replace with your MySQL server name
+  name                = "my-mysql-server"  # Replace with your MySQL server name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   administrator_login = "admin_user"  # Replace with your desired admin username
   administrator_password = "P@ssw0rd123!"  # Replace with your desired admin password
 
-  sku_name   = "Standard_D2s_v3"  # Replace with a valid SKU name for MySQL Flexible Server
+  sku   = "Standard_D2s_v3"  # Replace with a valid SKU name for MySQL Flexible Server
   version    = "5.7"
   tags = {
     environment = "development"
@@ -18,7 +18,7 @@ resource "azurerm_mysql_flexible_server" "example" {
 }
 
 resource "azurerm_mysql_flexible_database" "example" {
-  name                = "my_database"  # Replace with your database name
+  name                = "my-database"  # Replace with your database name
   resource_group_name = azurerm_resource_group.example.name
   server_name         = azurerm_mysql_flexible_server.example.name
   collation           = "utf8mb4_general_ci"
@@ -26,7 +26,7 @@ resource "azurerm_mysql_flexible_database" "example" {
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "my_app_service_plan"  # Replace with your app service plan name
+  name                = "my-app-service-plan"  # Replace with your app service plan name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku_name            = "B1"  # Example, replace with the appropriate SKU name
@@ -34,7 +34,7 @@ resource "azurerm_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "my_app_service"  # Replace with your app service name
+  name                = "my-app-service"  # Replace with your app service name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_service_plan.example.id
@@ -55,7 +55,7 @@ resource "azurerm_app_service" "example" {
 }
 
 resource "azurerm_static_web_app" "example" {
-  name                = "my_static_web_app"  # Replace with your static web app name
+  name                = "revhirestatic"  # Replace with your static web app name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 }
