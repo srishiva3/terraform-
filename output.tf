d50340b5047d7afa80e095796746fde27579d0d3
srishivajothish <81418992+srishiva3@users.noreply.github.com>
2024-06-04 08:34:00 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,7 @@
 output "mysql_connection_string" {
-  value = azurerm_mysql_flexible_server.example.fully_qualified_domain_name
+  value = azurerm_mysql_flexible_server.example.connection_strings[0].value
 }
+
 
 output "app_service_default_hostname" {
   value = azurerm_app_service.example.default_site_hostname
